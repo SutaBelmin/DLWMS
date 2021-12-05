fbfa19bc720495c7fb795cf02049e3e75d1e2a98
Adi Hodzic <hodzic.adi@edu.fit.ba>
2021-12-05 12:44:39 +0100

    Inicijalizacija projekta

---
+++ DLWMS_StudentskiOnlineServis/DLWMS_StudentskiOnlineServis/Data/DLWMS_baza.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DLWMS_StudentskiOnlineServis.Data
+{
+    public class DLWMS_baza : DbContext
+    {
+        public DLWMS_baza(DbContextOptions options) : base(options) { }
+
+    }
+}
